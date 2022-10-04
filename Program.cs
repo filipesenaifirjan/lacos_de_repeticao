21f3aeac37caa154ca99bd56c2e0b263544c0f0d
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-04 09:18:18 -0300

    construindo um menu interativo

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
-﻿
-int soma = 0, numero = 0; //as variáveis podem ser declaradas na mesma linha desde que sejam do mesmo tipo(inteiro)
+﻿string opcao;
 
-
-
-do  //vai executar o código independente da execução
+while(true)
 {
+    Console.Clear();//ele vai limpar o console
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - encerrar");
 
-Console.WriteLine("Digite um número:(0 para parar)");
-numero = Convert.ToInt32(Console.ReadLine());
+    opcao = Console.ReadLine();
 
- soma +=numero;
+switch(opcao) 
+{
+case "1":
+Console.WriteLine("Cadastro de Cliente");
+break;
+case "2":
+Console.WriteLine("Busca de Cliente");
+break;
+case "3":
+Console.WriteLine("Apagar de Cliente");
+break;
+case "4":
+Console.WriteLine("Encerrar");
+Environment.Exit(0);
+break;
+default:
 
+Console.WriteLine("Opção inválida");
+break;
+}
 
-} while(numero != 0); //!= significa diferente
-
-Console.WriteLine($"Total da soma dos números digitados é: {soma}");
-
+}
--- README.md
+++ README.md
@@ -64,4 +64,42 @@ numero = Convert.ToInt32(Console.ReadLine());
 } while(numero != 0); //!= significa diferente
 
 Console.WriteLine($"Total da soma dos números digitados é: {soma}");
--------------------------------------------------------------------------
+------------------------------------------------------------
+construindo um menu interativo:
+
+string opcao;
+
+while(true)
+{
+    Console.Clear();//ele vai limpar o console
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - encerrar");
+
+    opcao = Console.ReadLine();
+
+switch(opcao) 
+{
+case "1":
+Console.WriteLine("Cadastro de Cliente");
+break;
+case "2":
+Console.WriteLine("Busca de Cliente");
+break;
+case "3":
+Console.WriteLine("Apagar de Cliente");
+break;
+case "4":
+Console.WriteLine("Encerrar");
+Environment.Exit(0);
+break;
+default:
+
+Console.WriteLine("Opção inválida");
+break;
+}
+
+}
+-----------------------------------------------------
