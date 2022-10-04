c85083e08f5f081b052d7386872c097f3f2c3c04
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-04 08:06:22 -0300

    do while

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-﻿int numero = 5;
-int contador = 0;
+﻿
+int soma = 0, numero = 0; //as variáveis podem ser declaradas na mesma linha desde que sejam do mesmo tipo(inteiro)
 
 
-while (contador <=10)
+
+do  //vai executar o código independente da execução
 {
-Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-contador++;
 
-if (contador == 5)
-{
-    break;
-}
+Console.WriteLine("Digite um número:(0 para parar)");
+numero = Convert.ToInt32(Console.ReadLine());
 
-}
-//esse é mais simples por que ele recebe apenas a condição, porém precisa ficar esperto pra não cair em loop infinito
+ soma +=numero;
 
+
+} while(numero != 0); //!= significa diferente
+
+Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
--- README.md
+++ README.md
@@ -46,3 +46,22 @@ if (contador == 5)
 
 }
 -------------------------------------------------------------
+usando o do while:
+
+int soma = 0, numero = 0; //as variáveis podem ser declaradas na mesma linha desde que sejam do mesmo tipo(inteiro)
+
+
+
+do  //vai executar o código independente da execução
+{
+
+Console.WriteLine("Digite um número:(0 para parar)");
+numero = Convert.ToInt32(Console.ReadLine());
+
+ soma +=numero;
+
+
+} while(numero != 0); //!= significa diferente
+
+Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+-------------------------------------------------------------------------
